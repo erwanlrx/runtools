37ecbc69a1a1cc1a734395bbbf525c01e1599c2e
erleroux <erwan.le-roux@inria.fr>
2017-03-27 10:44:55 +0200

    update train_manager parameters

--- run/skeleton_sequences/run_train.py
+++ run/skeleton_sequences/run_train.py
@@ -2,6 +2,7 @@ from run.run_machine import RunGPU, RunCPU
 import sys, os
 
 SKELETON_SEQUENCE_TRAIN_PATH = '/home/lear/erleroux/src/skeleton_sequences/tensorflow_main'
+PYTHON_INTERPRETER = 'python3'
 
 
 def train_val_test_runs(run_argv, run_prefix, run_prefix_suffix, machine='gpu'):
@@ -14,9 +15,9 @@ def train_val_test_runs(run_argv, run_prefix, run_prefix_suffix, machine='gpu'):
         validation_run = RunGPUEvaluation(validation_run_argv, job_name)
         test_run = RunGPUEvaluation(test_run_argv, job_name)
     else:
-        train_run = RunGPUTrain(train_run_argv, job_name)
-        validation_run = RunGPUEvaluation(validation_run_argv, job_name)
-        test_run = RunGPUEvaluation(test_run_argv, job_name)
+        train_run = RunCPUTrain(train_run_argv, job_name)
+        validation_run = RunCPUEvaluation(validation_run_argv, job_name)
+        test_run = RunCPUEvaluation(test_run_argv, job_name)
     # Add dependencies
     validation_run.add_previous_run(train_run)
     test_run.add_previous_run(validation_run)
@@ -26,19 +27,19 @@ def train_val_test_runs(run_argv, run_prefix, run_prefix_suffix, machine='gpu'):
 class RunGPUTrain(RunGPU):
     def __init__(self, run_argv, job_name):
         RunGPU.__init__(self, run_argv)
-        self.python_interpreter = 'python3.5'  # tensorflow installed with python3
+        self.python_interpreter = PYTHON_INTERPRETER  # tensorflow installed with python3
         self.path_exe = os.path.join(SKELETON_SEQUENCE_TRAIN_PATH, 'train.py')
         self.job_name = job_name
 
     @property
     def oarsub_options(self):
-        return RunGPU(self).oarsub_options + ' -l "walltime=12:0:0"  -p "gpumem > 3200"'
+        return RunGPU(self).oarsub_options + ' -l "walltime=12:0:0" '
 
 
 class RunCPUTrain(RunCPU):
     def __init__(self, run_argv, job_name):
         RunCPU.__init__(self, run_argv)
-        self.python_interpreter = 'python3.5'  # tensorflow installed with python3
+        self.python_interpreter = PYTHON_INTERPRETER  # tensorflow installed with python3
         self.path_exe = os.path.join(SKELETON_SEQUENCE_TRAIN_PATH, 'train.py')
         self.job_name = job_name
 
@@ -50,7 +51,7 @@ class RunCPUTrain(RunCPU):
 class RunGPUEvaluation(RunGPU):
     def __init__(self, run_argv, job_name):
         RunGPU.__init__(self, run_argv)
-        self.python_interpreter = 'python3.5'  # tensorflow installed with python3
+        self.python_interpreter = PYTHON_INTERPRETER  # tensorflow installed with python3
         self.path_exe = os.path.join(SKELETON_SEQUENCE_TRAIN_PATH, 'evaluation.py')
         self.job_name = job_name
 
@@ -62,7 +63,7 @@ class RunGPUEvaluation(RunGPU):
 class RunCPUEvaluation(RunCPU):
     def __init__(self, run_argv, job_name):
         RunCPU.__init__(self, run_argv)
-        self.python_interpreter = 'python3.5'  # tensorflow installed with python3
+        self.python_interpreter = PYTHON_INTERPRETER  # tensorflow installed with python3
         self.path_exe = os.path.join(SKELETON_SEQUENCE_TRAIN_PATH, 'evaluation.py')
         self.job_name = job_name
 
--- run/skeleton_sequences/train_manager.py
+++ run/skeleton_sequences/train_manager.py
@@ -12,7 +12,7 @@ from itertools import product
 runs = []
 
 """ COPY AREA"""
-run_prefix = 'gpu_urnn'
+run_prefix = 'urnn_grid'
 idxs = [[0], [1], [0], [0]]
 
 restore = False
@@ -22,15 +22,16 @@ restore_checkpoint_filename = '10000'
 # Training setting
 for idx in product(*idxs):
     argv1 = main_argv(*idx)
-    argv1.extend(['run_prefix=' + run_prefix, 'training_steps=100', 'summary_flush_rate=10', 'checkpoint_rate=100'])
+    argv1.extend(['run_prefix=' + run_prefix, 'training_steps=20000', 'top_n_to_test=5',
+                  'summary_flush_rate=100', 'checkpoint_rate=1000'])
 
     # Training hyperparameters
     # TODO: add type of prediction, and regularization parameter
-    for batch_size, learning_rate in product([128], [1e-2]):
+    for batch_size, learning_rate in product([256], [1e-3]):
         argv2 = argv1[:] + ['batch_size=' + str(batch_size), 'learning_rate=' + str(learning_rate)]
 
         # Model hyperparameters
-        for rnn_units, rnn_layers, rnn_type in product([100], [1], ['lstm', 'gru']):
+        for rnn_units, rnn_layers, rnn_type in product([100], [1, 2], ['lstm', 'gru']):
             argv3 = argv2[:]
             argv3.extend(['rnn_units=' + str(rnn_units), 'rnn_layers=' + str(rnn_layers), 'rnn_type=' + rnn_type])
 
@@ -42,4 +43,5 @@ for idx in product(*idxs):
 
             # Extend the list of runs
             runs.extend(train_val_test_runs(argv3, run_prefix, run_prefix_suffix(argv3), machine='gpu'))
+# print(len(runs) / 3)
 manage(runs)
