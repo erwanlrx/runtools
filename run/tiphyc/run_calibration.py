575c79b1b59e0f78e4e71f488a474903478679c3
Erwan Le Roux <erwan.le-roux@univ-grenoble-alpes.fr>
2023-03-16 17:18:28 +0100

    update calibration script

--- run/tiphyc/run_calibration.py
+++ run/tiphyc/run_calibration.py
@@ -13,7 +13,7 @@ class AbstractRunTipHyc(RunCPU):
     def __init__(self, run_argv, job_name):
         RunCPU.__init__(self, run_argv)
         self.interpreter = local_interpreter
-        self.path_exe = os.path.join(TIPHYC_PATH, 'experiment/{}.py'.format(job_name))
+        self.path_exe = os.path.join(TIPHYC_PATH, 'analysis/trajectory/{}.py'.format(job_name))
         self.job_name = job_name
 
     @property
