aed4f698936270c7488a929a90a3d379d90fc5bd
Erwan Le Roux <erwan.le-roux@univ-grenoble-alpes.fr>
2023-08-28 17:50:08 +0200

    update run calibration tiphyc annual

--- run/tiphyc/run_calibration.py
+++ run/tiphyc/run_calibration.py
@@ -14,7 +14,7 @@ class AbstractRunTipHyc(RunCPU):
     def __init__(self, run_argv, analysis_folder, job_name):
         RunCPU.__init__(self, run_argv)
         self.interpreter = local_interpreter
-        self.path_exe = os.path.join(TIPHYC_PATH, 'analysis/{}/{}.py'.format(analysis_folder, job_name))
+        self.path_exe = os.path.join(TIPHYC_PATH, '{}/{}.py'.format(analysis_folder, job_name))
         self.job_name = job_name
 
     @property
@@ -31,35 +31,15 @@ class RunCalibrationWendling2019(AbstractRunTipHyc):
 class RunCalibrationTipHycAnnual(AbstractRunTipHyc):
 
     def __init__(self, run_argv):
-        super().__init__(run_argv, "trajectory", "main_tiphyc_annual")
+        super().__init__(run_argv, "projects/tiphyc_attribution/calibration_section",
+                         "main_trajectory_tiphyc_annual")
 
 
-class RunIndicatorComputation(AbstractRunTipHyc):
-
-    def __init__(self, run_argv):
-        super().__init__(run_argv, "stability_data", "main_save_stability_data")
-
-    @property
-    def oarsub_options(self):
-        return RunCPU(self).oarsub_options + ' -l "core=16,walltime=40:0:0"'
-
-
-def main_save_indicator():
-    short_sha = subprocess.check_output(['git', '--git-dir={}/.git'.format(TIPHYC_PATH),
-                                         'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-    for i in range(5, 8):
-        idx_watershed = str(i)
-        for j in [0, 9][1:]:
-            idx_final_year = str(j)
-            RunIndicatorComputation([short_sha, idx_watershed, idx_final_year]).run()
-
-
-def main_wendling2022_training():
-    for i in range(3):
+def main_tiphyc_annual_calibration():
+    for i in range(1):
         idx_watershed = str(i)
         RunCalibrationTipHycAnnual([idx_watershed]).run()
 
 
 if __name__ == '__main__':
-    main_save_indicator()
-    # main_wendling2022_training()
+    main_tiphyc_annual_calibration()
